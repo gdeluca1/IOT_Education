c7cfd890cae2278b0642cd488d2de174cc448550
Gennaro De Luca <gdeluca1@asu.edu>
2016-06-20 15:56:20 -0700

    Adding my own HCSR04 classes, based on Intel's

--- UltrasonicSensor.h
+++ UltrasonicSensor.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <exception>
 #include <mraa.hpp>
-#include <upm/hcsr04.h>
+#include <hcsr04.h>
 #include <functional>
 #include <stdexcept>
 #include <boost/thread/thread.hpp>
@@ -18,27 +18,16 @@ public:
 
 /* Start static members */
 
-	//TODO: Encapsulate this public member.  Ideally we don't need a global instance of the ultrasonic sensor, but the upm requires one for the callback,
-	//currently.  There was no way to bind (I tried std::bind) an HCSR04 instance to a function call.  I already submitted a pull request to upm
-	//proposing a change.
-	static upm::HCSR04 * ultrasonicSensor1;	
-	
-	//This is a necessary interrupt callback for the ultrasonic sensor	
-	static void interrupt1(void * args)
-	{
-		UltrasonicSensor::ultrasonicSensor1->ackEdgeDetected();
-	}
-
 	//This function is used to manage the static upm objects.
 	//TODO: Eliminate this and bind upm::HCSR04 objects to a callback, eliminating static objects and callbacks
-	static upm::HCSR04 * get_hcsr04(int trig_pin, int echo_pin)
+	static HCSR04 * get_hcsr04(int trig_pin, int echo_pin)
 	{
 
 		//find first NULL static member, and instantiate it
 		//instantiate the sensor and return the reference
 		if(UltrasonicSensor::ultrasonicSensor1 == NULL)
 		{
-			UltrasonicSensor::ultrasonicSensor1 = new upm::HCSR04(trig_pin, echo_pin, &UltrasonicSensor::interrupt1);
+			UltrasonicSensor::ultrasonicSensor1 = new upm::HCSR04(trig_pin, echo_pin);
 			return UltrasonicSensor::ultrasonicSensor1;
 		}
 		else
@@ -96,35 +85,12 @@ public:
 	float
 	value()
 	{
-		int attempts = 0;
-		value_func_struct f;
-		f.sensor_ref = this->m_ultrasonicSensor;
-		while(attempts < NUM_ATTEMPTS)
-		{
-			boost::thread t = boost::thread(boost::ref(f));
-			if(t.try_join_for(boost::chrono::milliseconds(TIMEOUT_MS)))
-			{
-				//we successfully got the value from the call
-				return f.read_value;
-			}
-			attempts++;
-			cout << "Failed to read from ultrasonic on attempt " << attempts << endl;
-
-			//Kill the thread that is stuck in an infinite loop
-			pthread_cancel(t.native_handle());
-		}
-
-		//The Intel upm library returns a value of 0 if a reading was unable to be obtained, we will do that as well.
-		//As we never want the students to have to ssh into the Edison, consistent we will not throw an exception here, though
-		//we could.
-		//throw runtime_error("Failed to get distance from distance sensor.");
-		//Consistent distance readings of 0 will indicate to the students that their ultrasonic sensor is beginning to degrade.
-		return 0; 
+		return this->m_ultrasonicSensor->getDistance(CM);
 	}
 
 protected:
 	int m_echo_pin, m_trig_pin;
-	upm::HCSR04 * m_ultrasonicSensor;
+	HCSR04 * m_ultrasonicSensor;
 
 private:
 
